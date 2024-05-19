b6dbb5a695f80025259ecd72a94aeb2a102c3a9f
Laurens van der Drift <Lauwy222@hotmail.com>
2024-05-19 16:21:03 +0200

    tussendoortje

--- Python opdracht 2/main.py
+++ Python opdracht 2/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+def buitenste_functie(boodschap):
+    def binnenste_functie():
+        print('Binnenste functie gestart')
+        print(boodschap)
+        print('Binnenste functie gestopt')
+    return binnenste_functie()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+hallo_func = buitenste_functie('Wazza')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+hallo_func()
